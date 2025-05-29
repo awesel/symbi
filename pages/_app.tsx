3ed762c027e38b127d3bef8e8ba909a236408e40
awesel <rcar670@gmail.com>
2025-05-29 15:09:13 -0700

    Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'; // Assuming you have a global stylesheet
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../contexts/AuthContext'; // Adjust path if your context is elsewhere
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AuthProvider>
   );
 }
