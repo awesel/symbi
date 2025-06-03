a10351966ecba01d8759d79e8f9e2e7194f36d98
awesel <rcar670@gmail.com>
2025-06-03 16:44:59 -0700

    Fix social proof

--- pages/login.tsx
+++ pages/login.tsx
@@ -88,7 +88,7 @@ const LoginPage: React.FC = () => {
                 <img src="/images/peter.png" alt="User Avatar" />
              </div>
             <p className={styles.trustBadge}>
-              Join 50+ people already onboard
+              Join 50+ Stanford students already onboard
             </p>
           </div>
         </section>
